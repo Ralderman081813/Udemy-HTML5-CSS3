8b5e96027a9712ad28afebf0b9f7dbf417a29014
Reissa Alderman <reissa.alderman@gmail.com>
2019-05-15 19:02:24 -0400

    For Rachael
    For Rachael

--- ProjectFiles/resources/scripts/scripts.js
+++ ProjectFiles/resources/scripts/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    
+    $(".features--js").waypoint(function(direction) {
+       if (direction == "down") {
+           $("nav").addClass("sticky");
+       } else {
+           $("nav").removeClass("sticky");
+       }
+    });
+    /*
+    $('#handler-first').waypoint(function(direction) {
+      notify(this.element.id + ' hit 25% from top of window') 
+    }, {
+      offset: '25%'
+    })
+    */
 });
