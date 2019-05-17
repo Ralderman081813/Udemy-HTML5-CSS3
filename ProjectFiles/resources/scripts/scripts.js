f69c4e72c74c17b8f06ae27a604f2c832de8e271
Reissa Alderman <reissa.alderman@gmail.com>
2019-05-16 21:26:48 -0400

    Animations
    Added animations to elements

--- ProjectFiles/resources/scripts/scripts.js
+++ ProjectFiles/resources/scripts/scripts.js
@@ -1,16 +1,41 @@
 $(document).ready(function() {
-    $(".features--js").waypoint(function(direction) {
+    /* For the sticky nav */
+    $("#features-section").waypoint(function(direction) {
        if (direction == "down") {
            $("nav").addClass("sticky");
        } else {
            $("nav").removeClass("sticky");
        }
     });
-    /*
-    $('#handler-first').waypoint(function(direction) {
-      notify(this.element.id + ' hit 25% from top of window') 
+    /* Scroll on button click */
+    $(".js-scroll-plans").click(function () {
+        $("html, body").animate({scrollTop: $("#plans-section")
+.offset().top});
+    });
+    $(".js-scroll-features").click(function () {
+        $("html, body").animate({scrollTop: $("#features-section")
+.offset().top});
+    });
+    
+    /* Animations on scroll */
+    $(".animation-1").waypoint(function(direction) {
+        $(".animation-1").addClass('animated fadeIn');
     }, {
-      offset: '25%'
-    })
-    */
+        offset: "50%"
+    });
+    $(".animation-2").waypoint(function(direction) {
+        $(".animation-2").addClass('animated fadeInUp');
+    }, {
+        offset: "50%"
+    });
+    $(".animation-3").waypoint(function(direction) {
+        $(".animation-3").addClass('animated flipInY');
+    }, {
+        offset: "50%"
+    });
+    $(".animation-4").waypoint(function(direction) {
+        $(".animation-4").addClass('animated pulse');
+    }, {
+        offset: "50%"
+    });
 });
